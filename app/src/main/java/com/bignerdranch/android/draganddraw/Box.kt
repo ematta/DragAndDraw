64226641043033a0c699457aa7e53ccde7cd922e
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-02-20 18:14:35 -0600

    ch 23

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/Box.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.draganddraw
+
+import android.graphics.PointF
+
+data class Box(val start: PointF){
+    var end: PointF = start
+
+    val left: Float
+        get () = start.x.coerceAtMost(end.x)
+
+    val right: Float
+        get() = start.x.coerceAtLeast(end.x)
+
+    val top: Float
+        get() = start.y.coerceAtMost(end.y)
+
+    val bottom: Float
+        get() = start.y.coerceAtLeast(end.y)
+}
